3e76f14909829ee8e8eb5b8c54e4c84e3f885fab
Andy Gocke <andy@commentout.net>
2024-10-14 16:14:31 -0700

    WIP

---
+++ src/tools/GenerateResxSource/Program.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+var resxGen = new Microsoft.DotNet.Arcade.GenerateResxSource()
+{
+    Language = "C#",
+    OutputPath = "",
+    ResourceName = "",
+    ResourceFile = "",
+};
+resxGen.Execute();
+Console.WriteLine("Hello, World!");
