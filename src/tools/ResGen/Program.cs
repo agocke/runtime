35bbe44bd08e62ed7eb77543905432ea2c374c46
Andy Gocke <andy@commentout.net>
2024-10-17 16:03:27 -0700

    Update ResGen

--- src/tools/ResGen/Program.cs
+++ src/tools/ResGen/Program.cs
@@ -5,84 +5,25 @@ using Microsoft.Build.Tasks;
 
 Console.WriteLine("Hello, World!");
 
-ProcessResourceFiles process = new ProcessResourceFiles();
+var srcPath = "/home/andy/code/runtime/src/libraries/System.Console/src/Resources/Strings.resx";
+string[] _references = [
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/Microsoft.Win32.Primitives.dll",
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/System.Collections.dll",
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/System.Memory.dll",
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/System.Runtime.dll",
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/System.Runtime.InteropServices.dll",
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/System.Text.Encoding.Extensions.dll",
+    "/home/andy/code/runtime/artifacts/bin/microsoft.netcore.app.ref/ref/net9.0/System.Threading.dll",
+];
+var outPath = "/home/andy/code/runtime/artifacts/obj/System.Console/Debug/net9.0-unix/FxResources.System.Console.SR.resources";
 
-int _references = 0;
-int inputsToProcess = 0;
-int _satelliteInputs = 0;
-int outputsToProcess = 0;
-bool outOfProcExecutionSucceeded = false;
-bool _stronglyTypedResourceSuccessfullyCreated = false;
-bool UseSourcePath = false;
-bool UsePreserializedResources = false;
-string _stronglyTypedNamespace = "";
-string StronglyTypedLanguage = "";
-string _stronglyTypedManifestPrefix = "";
-string StronglyTypedFileName = "";
-string StronglyTypedClassName = "";
-bool PublicClass = false;
-bool ExtractResWFiles = false;
-string OutputDirectory = "";
-bool WarnOnBinaryFormatterUse = false;
 
-process.Run(
-    _references,
-    inputsToProcess,
-    _satelliteInputs,
-    outputsToProcess,
-    UseSourcePath,
-    UsePreserializedResources,
-    StronglyTypedLanguage,
-    _stronglyTypedNamespace,
-    _stronglyTypedManifestPrefix,
-    StronglyTypedFileName,
-    StronglyTypedClassName,
-    PublicClass,
-    ExtractResWFiles,
-    OutputDirectory,
-    WarnOnBinaryFormatterUse);
+var logger = new Logger();
+var process = new ProcessResourceFiles(
+    logger,
+    srcPath,
+    outPath,
+    _references);
+process.Run();
 
-this.StronglyTypedClassName = process.StronglyTypedClassName; // in case a default was chosen
-this.StronglyTypedFileName = process.StronglyTypedFilename;   // in case a default was chosen
-_stronglyTypedResourceSuccessfullyCreated = process.StronglyTypedResourceSuccessfullyCreated;
-if (process.UnsuccessfullyCreatedOutFiles != null)
-{
-    foreach (string item in process.UnsuccessfullyCreatedOutFiles)
-    {
-        _unsuccessfullyCreatedOutFiles.Add(item);
-    }
-}
-
-if (ExtractResWFiles)
-{
-    ITaskItem[] outputResources = process.ExtractedResWFiles.ToArray();
-
-    if (cachedOutputFiles.Count > 0)
-    {
-        OutputResources = new ITaskItem[outputResources.Length + cachedOutputFiles.Count];
-        outputResources.CopyTo(OutputResources, 0);
-        cachedOutputFiles.CopyTo(OutputResources, outputResources.Length);
-    }
-    else
-    {
-        OutputResources = outputResources;
-    }
-
-    // Get portable library cache info (and if needed, marshal it to this AD).
-    List<ResGenDependencies.PortableLibraryFile> portableLibraryCacheInfo = process.PortableLibraryCacheInfo;
-    for (int i = 0; i < portableLibraryCacheInfo.Count; i++)
-    {
-        _cache.UpdatePortableLibrary(portableLibraryCacheInfo[i]);
-    }
-}
-
-// And now we serialize the cache to save our resgen linked file resolution for later use.
-WriteStateFile();
-
-RemoveUnsuccessfullyCreatedResourcesFromOutputResources();
-
-RecordFilesWritten();
-
-//MSBuildEventSource.Log.GenerateResourceOverallStop();
-
-return !Log.HasLoggedErrors;
+return logger.HasLoggedErrors ? 1 : 0;
